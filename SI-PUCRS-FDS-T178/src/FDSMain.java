9a60dc0b47fc81513e994f914576f5702b908542
antoniommguedes <antonio.miguel@acad.pucrs.br>
2017-08-29 22:40:06 -0300

    Antonioguedes

---
+++ SI-PUCRS-FDS-T178/src/FDSMain.java
@@ -0,0 +1,34 @@
+
+public class FDSMain {
+
+	public static void main(String[] args) {
+		// *******************
+		// Insira, no final deste m�todo, um coment�rio com
+		// o seu nome, e um println com o Hello, World, conform
+		// o exemplo abaixo.
+		System.out.println("Hello, World! Comunitario!\n");
+
+		// Michael da Costa M�ra
+		System.out.println("Hello, World! Comunitario! Michael\n");
+		
+		// Wagner Santos
+		System.out.println("Hello, World! Comunitario! Wagner\n");
+
+		// Franco Fiorini
+		System.out.println("Hello, World! Comunitario! Franco\n");
+
+		//Kim Schaurich
+		System.out.println("Hello, World! Comunitario! Kim Schaurich\n");
+		
+		// Thiago Roly Gomes
+		System.out.println("Hello, World! Comunitario! Thiago\n");
+		
+		//Catarina Nogueira
+		System.out.println("Hello, World! Comunitario! Catarina\n");
+		
+		//Antonio Guedes
+		System.out.println("Hello, World! Comunitario! Antonio\n");
+
+	}
+
+}
